d74f49335c44c75a86e3006a0741b9bf498c583c
rodrigobarr0s <rodrigobarros.dev@gmail.com>
2025-11-20 08:49:10 -0300

    chore(project): setup inicial com Spring Boot 3.x
    - Criação do projeto base
    - Configuração inicial de dependências

---
+++ src/main/java/io/github/rodrigobarr0s/access_modules_api/AccessModulesApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.rodrigobarr0s.access_modules_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessModulesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessModulesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/rodrigobarr0s/access_modules_api/AccessModulesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rodrigobarr0s.access_modules_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessModulesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
